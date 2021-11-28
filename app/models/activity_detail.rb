a401c87ccaa5d3bb73d107197568c2727dd72206
Kailani Chu <me@kailani.io>
2021-11-27 23:24:20 -1000

    Update model data validation

--- app/models/activity_detail.rb
+++ app/models/activity_detail.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ActivityDetail < ApplicationRecord
-  validates :activity, presence: true
+  validates :name, presence: true
   validates :hours, presence: true, numericality: { greater_than_or_equal_to: 0 }
   has_many :hour_entries
 end
